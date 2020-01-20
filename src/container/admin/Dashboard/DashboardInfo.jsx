f274efe03211c92f5c2dd201472bf955a8ca7551
masaditya <aeka489@gmail.com>
2020-01-19 22:56:50 -0800

    fix::chartjs jsx

--- src/container/admin/Dashboard/DashboardInfo.jsx
+++ src/container/admin/Dashboard/DashboardInfo.jsx
@@ -1,8 +1,20 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
+import { Doughnut, Bar, Pie } from 'react-chartjs-2'
 
 export class DashboardInfo extends Component {
     render() {
+
+        const data = {
+
+            options: {
+                legend: {
+                    display: false
+                },
+                cutoutPercentage: 80
+            }
+        }
+
         return (
             <div>
                 <div className="container">
@@ -27,7 +39,19 @@ export class DashboardInfo extends Component {
                     <div className="row">
                         <div className="col-md-6 ">
                             <div className="bg-white w-100 py-3 shadow-sm rounded row justify-content-center">
-                                <canvas id="user-chart-doughnut" className="w-25 h-25" />
+                                {/* <canvas id="user-chart-doughnut" className="w-25 h-25" /> */}
+                                <div className="w-25 h-25">
+                                    <Doughnut data={{
+                                        labels: ['User'],
+                                        datasets: [{
+                                            data: [45],
+                                            backgroundColor: '#05a4bd',
+                                            borderColor: '#05a4bd'
+                                        }]
+                                    }}
+                                        options={data.options}
+                                    />
+                                </div>
                                 <div className="fz-1 align-items-center my-auto">
                                     <b className="fz-2">All User</b>
                                     <p className="text-gray m-0"> 100.00% Users</p>
@@ -36,7 +60,19 @@ export class DashboardInfo extends Component {
                         </div>
                         <div className="col-md-6 ">
                             <div className="bg-white border-dashed w-100 py-3 rounded shadow-sm row justify-content-center">
-                                <canvas id="segment-chart-doughnut" className="w-25 h-25" />
+                                {/* <canvas id="segment-chart-doughnut" className="w-25 h-25" /> */}
+                                <div className="w-25 h-25">
+                                    <Doughnut data={{
+                                        labels: ['User'],
+                                        datasets: [{
+                                            data: [45],
+                                            borderColor: '#dedfe1'
+                                        }]
+
+                                    }}
+                                        options={data.options}
+                                    />
+                                </div>
                                 <div className="fz-1 align-items-center my-auto">
                                     <b className="fz-2 "> <a href className="text-gray">+ Add Segment</a> </b>
                                 </div>
@@ -68,13 +104,78 @@ export class DashboardInfo extends Component {
                         <div className="col-md-5">
                             <div className="bg-white w-100 p-4 pb-5 shadow-sm">
                                 <p className="pb-3 font-weight-bold">Top Channels</p>
-                                <canvas id="top-channel-pie" />
+                                {/* <canvas id="top-channel-pie" /> */}
+                                <Pie data={{
+                                    labels: ['Direct', "Organic Search", 'Social', 'Referal'],
+                                    datasets: [{
+                                        data: [59, 39, 10, 15],
+                                        borderWidth: 0,
+                                        backgroundColor: [
+                                            '#05a4bd', '#b6eded', '#35e69b', '#f7e686'
+                                        ]
+                                    }]
+                                }}
+                                    options={{
+                                        legend: {
+                                            position: "right",
+                                            labels: {
+                                                boxWidth: 12
+                                            }
+                                        },
+                                        tooltips: {
+                                            enabled: false
+                                        }
+                                    }}
+                                />
                             </div>
                         </div>
                         <div className="col-md-7">
                             <div className="bg-white w-100 p-4 shadow-sm">
                                 <p className="pb-3 font-weight-bold">Users</p>
-                                <canvas className="w-75 h-auto mx-auto" id="users-pie" />
+                                {/* <canvas className="w-75 h-auto mx-auto" id="users-pie" /> */}
+                                <div className="w-75 h-auto mx-auto">
+                                    <Pie
+                                        data={{
+                                            labels: ['User'],
+                                            datasets: [{
+                                                // backgroundColor: gradient, // Put the gradient here as a fill color
+                                                borderColor: "#05a4bd",
+                                                pointBackgroundColor: "#ffffff",
+                                                pointBorderWidth: 2,
+                                                pointRadius: 8,
+                                                pointBorderColor: "#05a4bd",
+                                                data: [14, 115, 13]
+                                            }]
+                                        }}
+
+                                        options={{
+                                            legend: {
+                                                display: false
+                                            },
+                                            elements: {
+                                                line: {
+                                                    tension: 0
+                                                }
+                                            },
+                                            scales: {
+                                                xAxes: [{
+                                                    display: true
+                                                }],
+                                                yAxes: [{
+                                                    gridLines: {
+                                                        display: true
+                                                    },
+                                                    ticks: {
+                                                        display: false
+                                                    }
+                                                }],
+                                            },
+                                            responsive: true,
+
+                                        }}
+                                    />
+                                </div>
+
                             </div>
                         </div>
                     </div>
@@ -161,7 +262,19 @@ export class DashboardInfo extends Component {
                     <div className="row mt-3 p-0 mx-0">
                         <div className="col-md-6 ">
                             <div className="bg-white mr-0 w-100 py-3 shadow-sm rounded row justify-content-center">
-                                <canvas id="user-chart-doughnut2" className="w-25 h-25" />
+                                {/* <canvas id="user-chart-doughnut2" className="w-25 h-25" /> */}
+                                <div className="w-25 h-25">
+                                    <Doughnut data={{
+                                        labels: ['User'],
+                                        datasets: [{
+                                            data: [45],
+                                            backgroundColor: '#05a4bd',
+                                            borderColor: '#05a4bd'
+                                        }]
+                                    }}
+                                        options={data.options}
+                                    />
+                                </div>
                                 <div className="fz-1 align-items-center my-auto">
                                     <b className="fz-2">All User</b>
                                     <p className="text-gray m-0"> 100.00% Users</p>
@@ -170,7 +283,18 @@ export class DashboardInfo extends Component {
                         </div>
                         <div className="col-md-6 ">
                             <div className="bg-white mr-0 w-100 py-3 border-dashed  rounded shadow-sm row justify-content-center">
-                                <canvas id="segment-chart-doughnut2" className="w-25 h-25" />
+                                {/* <canvas id="segment-chart-doughnut2" className="w-25 h-25" /> */}
+                                <div className="w-25 h-25">
+                                    <Doughnut data={{
+                                        labels: ['User'],
+                                        datasets: [{
+                                            data: [45],
+                                            borderColor: '#dedfe1'
+                                        }]
+                                    }}
+                                        options={data.options}
+                                    />
+                                </div>
                                 <div className="fz-1 align-items-center my-auto">
                                     <b className="fz-2 "> <a href className="text-gray">+ Add Segment</a> </b>
                                 </div>
@@ -188,12 +312,65 @@ export class DashboardInfo extends Component {
                     <div className="row bg-white">
                         <div className="col-md-5 p-3">
                             <b>Age</b>
-                            <canvas className="mt-4" id="age-bar" />
+                            {/* <canvas className="mt-4" id="age-bar" /> */}
+                            <div className="mt-4">
+                                <Bar
+                                    data={{
+                                        labels: ['18-24', "25-34", "34-30", '41-50', '51-65', '65+'],
+                                        datasets: [{
+                                            data: [27, 38, 9, 5, 4, 2],
+                                            borderColor: '#dedfe1',
+                                            backgroundColor: "#05a4bd"
+                                        }]
+                                    }}
+                                    options={{
+                                        legend: {
+                                            display: false
+                                        },
+                                        scales: {
+                                            xAxes: [{
+                                                gridLines: {
+                                                    display: false
+                                                }
+                                            }],
+                                            yAxes: [{
+                                                gridLines: {
+                                                    display: true
+                                                }
+                                            }]
+                                        }
+                                    }}
+                                />
+                            </div>
                         </div>
                         <div className="col-md-7 p-3">
                             <b className="text-left">Gender</b>
                             <span className="fz-1  float-right text-black-50">0,26% of total Users</span>
-                            <canvas className="w-75 h-auto" id="gender-pie" />
+                            <div className="w-75 h-auto" >
+                                <Pie
+                                    data={{
+                                        labels: ['Male', "Female"],
+                                        datasets: [{
+                                            data: [59, 39],
+                                            borderWidth: 0,
+                                            backgroundColor: [
+                                                '#05a4bd', '#b6eded'
+                                            ]
+                                        }]
+                                    }}
+                                    options={{
+                                        legend: {
+                                            position: "right",
+                                            labels: {
+                                                boxWidth: 12
+                                            }
+                                        },
+                                        tooltips: {
+                                            enabled: false
+                                        }
+                                    }}
+                                />
+                            </div>
                             <div className="text-right mt-4">
                                 <span className="fz-1 text-black-50">This report was generated on 3/15/20 at 01:00:22 PM</span>
                                 <a className="fz-1 ml-4" href>Refersh Report</a>
